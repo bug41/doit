1064db84b89b01e6523f7b493f168a2cc5afcb7d
bugkingus <bugkingus@gmail.com>
2023-03-16 07:47:38 +0900

    크기 설정 위젯 활용

--- lib/ch10_3_intricsic_constrained/test.dart
+++ lib/ch10_3_intricsic_constrained/test.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
 
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+        ),
+        body: Column(
+          children: [
+            IntrinsicWidth(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(color: Colors.red, width: 50, height: 50),
+                  Container(color: Colors.green, width: 150, height: 150),
+                  Container(color: Colors.blue, width: 100, height: 100),
+                ],
+              ),
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                minWidth: 300,
+                maxHeight: 50,
+              ),
+              child: Container(
+                color: Colors.amber,
+                width: 150,
+                height: 150,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
