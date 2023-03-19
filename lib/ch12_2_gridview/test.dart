145dd10e63eee7adb4443cbe3320514de486279b
bugkingus <bugkingus@gmail.com>
2023-03-20 08:50:58 +0900

    그리드 뷰 활용하기

---
+++ lib/ch12_2_gridview/test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<String> citys = ['서울시', '인천시', '부산시', '대구시', '대전시', '광주시', '울산시'];
+
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: [Text(citys[index]), Image.asset('images/big.jpeg')],
+              ),
+            );
+          },
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
