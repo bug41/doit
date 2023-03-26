fa3d2a2dc34ac5ee5a6c44c62b07e82659135391
bug41 <bugkinguswit@gmail.com>
2023-03-26 19:09:49 +0900

    라우트 화면 전환

---
+++ lib/ch14_1_navigation/main.dart
@@ -0,0 +1,26 @@
+import 'package:doit/ch14_1_navigation/four_screen.dart';
+import 'package:doit/ch14_1_navigation/one_screen.dart';
+import 'package:doit/ch14_1_navigation/three_screen.dart';
+import 'package:doit/ch14_1_navigation/two_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/one',
+      routes: {
+        '/one': (context) => const OneScreen(),
+        '/two': (context) => const TwoScreen(),
+        '/three': (context) => const ThreeScreen(),
+        '/four': (context) => const FourScreen(),
+      },
+    );
+  }
+}
