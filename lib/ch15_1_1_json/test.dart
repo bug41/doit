98a91cba113a3d35eeeb4d76ad7e2507ab42609f
bugkingus <bugkingus@gmail.com>
2023-03-29 08:06:18 +0900

    JSON 데이터 파싱하기

--- lib/ch15_1_1_json/test.dart
+++ lib/ch15_1_1_json/test.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<StatefulWidget> createState() => MyAppState();
 }
 
 class Todo {
@@ -16,18 +21,58 @@ class Todo {
   Todo(this.id, this.title, this.completed);
 
   Todo.fromJson(Map<String, dynamic> json)
-    : id = json['id'],
-      title = json['title'],
-      completed = json['completed'],
-  
-  Map<String , dynamic> toJson() => {
-    'id': id, 'title' : title, 'completed': completed
-  };
+      : id = json['id'],
+        title = json['title'],
+        completed = json['completed'];
+
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'title': title, 'completed': completed};
 }
 
-class _MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
+  String jsonStr = '{"id" : 1, "title": "HELLO", "completed": false}';
+  Todo? todo;
+  String result = '';
+
+  onPressDecode() {
+    Map<String, dynamic> map = jsonDecode(jsonStr);
+    todo = Todo.fromJson(map);
+    setState(() {
+      result =
+          "decode: id: ${todo?.id}, title: ${todo?.title}, completed: ${todo?.completed}";
+    });
+  }
+
+  onPressEncode() {
+    setState(() {
+      result = "encode: ${jsonEncode(todo)}";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(result),
+              ElevatedButton(
+                onPressed: onPressDecode,
+                child: const Text('Decode'),
+              ),
+              ElevatedButton(
+                onPressed: onPressEncode,
+                child: const Text('Encode'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
