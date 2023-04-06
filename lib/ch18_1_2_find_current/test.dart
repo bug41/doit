3961c5e663f2fcbe18263a2e192247f6d7285dfe
bugkingus <bugkingus@gmail.com>
2023-04-06 09:13:02 +0900

    하트 왜 count 안돼 아오

--- lib/ch18_1_2_find_current/test.dart
+++ lib/ch18_1_2_find_current/test.dart
@@ -7,15 +7,135 @@ void main(List<String> args) {
 class ParentWidget extends StatefulWidget {
   const ParentWidget({super.key});
 
-  const ParentWidget({super.key});
-
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<ParentWidget> createState() => ParentWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class ParentWidgetState extends State<ParentWidget> {
+  bool favorited = false;
+  int favoriteCount = 10;
+  GlobalKey<ChildWidgetState> childKey = GlobalKey();
+  int childCount = 0;
+
+  void toggleFavorite() {
+    setState(() {
+      print('왜 여기 안와');
+      if (favorited) {
+        favoriteCount -= 1;
+        favorited = false;
+      } else {
+        favoriteCount += 1;
+        favorited = true;
+      }
+    });
+  }
+
+  void getChildData() {
+    ChildWidgetState? childState = childKey.currentState;
+    setState(() {
+      childCount = childState?.childCount ?? 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('State Test'),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Text('I am Parent, child count : $childCount'),
+                    ),
+                    ElevatedButton(
+                      onPressed: getChildData,
+                      child: const Text('get child data'),
+                    )
+                  ],
+                ),
+                ChildWidget(key: childKey),
+                const IconWidget(),
+                const ContentWidget()
+              ],
+            )));
+  }
+}
+
+class ChildWidget extends StatefulWidget {
+  const ChildWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ChildWidgetState();
+  }
+}
+
+class ChildWidgetState extends State<ChildWidget> {
+  int childCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text('I am Child, $childCount'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              childCount++;
+            });
+          },
+          child: const Text('increment'),
+        ),
+      ],
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  const IconWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    ParentWidgetState? state =
+        context.findAncestorStateOfType<ParentWidgetState>();
+
+    return Center(
+      child: IconButton(
+        icon: ((state?.favorited ?? false)
+            ? const Icon(Icons.favorite)
+            : const Icon(Icons.favorite_border)),
+        color: Colors.blue,
+        iconSize: 100,
+        onPressed: state?.toggleFavorite,
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  const ContentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ParentWidgetState? state =
+        context.findAncestorStateOfType<ParentWidgetState>();
+
+    return Center(
+      child: Text(
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        'favoriteCount : ${state?.favoriteCount}',
+      ),
+    );
   }
 }
