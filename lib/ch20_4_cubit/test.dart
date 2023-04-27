1d41a993d551cf668be3a755c5868bfdce648fc3
bugkingus <bugkingus@gmail.com>
2023-04-28 08:29:31 +0900

    상태관리 라이브러리 큐빗

---
+++ lib/ch20_4_cubit/test.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+}
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cubit Test'),
+        ),
+        body: BlocProvider(
+          create: (_) => CounterCubit(),
+          child: const MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var cubit = BlocProvider.of<CounterCubit>(context);
+    return BlocBuilder<CounterCubit, int>(
+      builder: (context, count) {
+        return Container(
+          color: Colors.deepOrange,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Cubit : $count',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('increment'),
+                  onPressed: () {
+                    cubit.increment();
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('decrement'),
+                  onPressed: () {
+                    cubit.decrement();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
